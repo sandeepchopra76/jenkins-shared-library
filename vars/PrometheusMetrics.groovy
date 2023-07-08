61339b1aabcf66563451a724fc957a47bf7b68d1
Sandeep Chopra <sandeepchopra76@gmail.com>
2023-07-09 00:52:33 +0530

    fixed file overwriting issue

--- vars/PrometheusMetrics.groovy
+++ vars/PrometheusMetrics.groovy
@@ -3,6 +3,7 @@
 def call(String stageName, long startTime, long endTime, String buildResult) {
     def jobName = env.JOB_NAME.replace('/', '_')
     def buildId = env.BUILD_ID
+    def filePath = "${env.WORKSPACE}/prometheus_metrics.txt"
 
     def metric = """
         # TYPE build_stage_duration_seconds gauge
@@ -11,8 +12,17 @@ def call(String stageName, long startTime, long endTime, String buildResult) {
         # TYPE build_stage_status gauge
         build_stage_status{job="${jobName}", build="${buildId}", stage="${stageName}"} ${buildResult}
     """
+    
+    // Check if the file exists
+    def fileExists = fileExists(filePath)
 
-    writeFile(file: "${env.WORKSPACE}/prometheus_metrics.txt", text:metric, append: true)
+    if (fileExists) {
+        // Append the metric to the existing file
+        appendFile(file: filePath, text: metric)
+    } else {
+        // Create a new file and write the metric content
+        writeFile(file: filePath, text: metric)
+    }
 }
 
 
