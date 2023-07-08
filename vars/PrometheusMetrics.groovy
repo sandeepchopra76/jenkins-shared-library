3ae7da551c7f126de3f549b8898f2f652d76f519
Sandeep Chopra <sandeepchopra76@gmail.com>
2023-07-08 14:47:43 +0530

    taken out code for build time metrics

--- vars/PrometheusMetrics.groovy
+++ vars/PrometheusMetrics.groovy
@@ -6,14 +6,9 @@ def call() {
             def jobName = env.JOB_NAME.replace('/', '_')
             def buildId = env.BUILD_ID
             def stageName = env.STAGE_NAME
-            def startTime = env.BUILD_TIMESTAMP?.toInstant()?.toEpochMilli()
-            def endTime = System.currentTimeMillis()
             def status = currentBuild.result == 'SUCCESS' ? 1 : 0
 
             def metric = """
-                # TYPE build_stage_duration_seconds gauge
-                build_stage_duration_seconds{job="${jobName}", build="${buildId}", stage="${stageName}"} ${endTime - startTime}
-                
                 # TYPE build_stage_status gauge
                 build_stage_status{job="${jobName}", build="${buildId}", stage="${stageName}"} ${status}
             """
